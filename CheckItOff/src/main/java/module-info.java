e32ec5328e1b76c85de31f6ac0c2abe24e53e61b
Jaunius <jausil@ktu.lt>
2023-02-28 13:49:03 +0200

    Setting up the JavaFX project

---
+++ CheckItOff/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.checkitoff {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.checkitoff to javafx.fxml;
+    exports com.example.checkitoff;
+}
