3c412afc2d255c67acab05d12a5b005fe4c61708
Jaunius <jausil@ktu.lt>
2023-03-15 14:06:48 +0200

    Atlikta užduoties forma ir pridėta naujas AnchorPane užduotims laikyti

--- CheckItOfHtml/src/CheckItOff/src/main/java/com/example/checkitoff/TaskController.java
+++ CheckItOfHtml/src/CheckItOff/src/main/java/com/example/checkitoff/TaskController.java
@@ -2,43 +2,94 @@ package com.example.checkitoff;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.AnchorPane;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
 
-public class TaskController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TaskController implements Initializable {
 
     @FXML
     private AnchorPane AddBar;
-
     @FXML
     private Button AddNewTask;
-
     @FXML
     private MenuItem Calendar;
-
     @FXML
     private Label Description;
-    
     @FXML
     private ListView<?> MenuList;
-
     @FXML
     private Button SaveButton;
-
     @FXML
     private Label TaskName;
-
     @FXML
     private MenuItem Today;
-
     @FXML
     private MenuItem Upcoming;
+    @FXML
+    private TextArea textAreaDescription;
+    @FXML
+    private TextField textFieldName;
+
+    @FXML
+    private AnchorPane taskList;
+
+    private int taskCount = 0;
 
     @FXML
     void onAddButtonClick(ActionEvent event) {
-            
+        //Sets the Task form visible if its already not
+        if (!AddBar.isVisible()) {
+            AddBar.setVisible(true);
+        }
+    }
+
+    @FXML
+    public void onSaveButtonClick(ActionEvent actionEvent) {
+        //Sets the Task form invisible if its already visible
+        if (AddBar.isVisible()) {
+            AddBar.setVisible(false);
+        }
+
+        //Data transfer to the database
+
+        //Adding task with given data
+
+        //Form cleanup
+        textFieldName.clear();
+        textAreaDescription.clear();
+    }
+    //helping method loads all checkBox elements with a label text next to it and a status
+    // to a taskList AnchorPane.
+    public void loadTasks(String[] label, boolean[] status) {
+        taskList.getChildren().clear();
+        for(int i = 0; i < label.length; i++)
+        {
+            CheckBox checkBox = new CheckBox(label[i]);
+            checkBox.setSelected(status[i]);
+            taskList.getChildren().add(checkBox);
+            Node node = taskList.getChildren().get(taskCount);
+            node.setLayoutY(20 * taskCount);
+            taskCount++;
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Setting the Task form to be invisible
+        AddBar.setVisible(false);
+
+        //Loading DataBase data to selected menu type.
+
+        //DataBase imitation
+        String[] DataBase_TaskName = {"Do laundry", "Do dishes"};
+        boolean[] DataBase_TaskCompletion = {true, false};
+
+        //Loading Tasks
+        loadTasks(DataBase_TaskName, DataBase_TaskCompletion);
     }
 }
