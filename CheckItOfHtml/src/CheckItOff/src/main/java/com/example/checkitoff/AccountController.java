5ae8405b980179d6bfd4a550fe70b2262903c28d
rutike24 <105065547+rutike24@users.noreply.github.com>
2023-04-08 11:39:47 +0300

    Sukurti prisijungimo ir registracijos controller
    Reikia suderinti DB pavadinimus, kad nebūtų klaidų ir patikrinti, ar veikia

---
+++ CheckItOfHtml/src/CheckItOff/src/main/java/com/example/checkitoff/AccountController.java
@@ -0,0 +1,45 @@
+package com.example.checkitoff;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AccountController implements Initializable {
+    @FXML
+    private Button button_login;
+
+    @FXML
+    private Button button_sign_up;
+
+    @FXML
+    private TextField tf_password;
+
+    @FXML
+    private TextField tf_username;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.logInUser(event, tf_username.getText(), tf_password.getText());
+            }
+        });
+
+        button_sign_up.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "signup.fxml", "Sign Up!", null);
+            }
+        });
+
+        }
+    }
+}
